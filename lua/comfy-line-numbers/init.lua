ee125296aca149e2a53f26457a43edcb630e99ff
Miles Luders <mpluders@gmail.com>
2024-08-27 22:07:12 -0700

    Provide user commands for enabling/disabling

--- lua/comfy-line-numbers/init.lua
+++ lua/comfy-line-numbers/init.lua
@@ -3,6 +3,10 @@
 -- clear the cache with the following command
 -- `:lua package.loaded['plugin-template'] = nil`
 
+local AUTOCMD_GROUP = 'comfy-signs'
+
+local enabled = false
+
 local labels = {
   '1',
   '2',
@@ -53,11 +57,15 @@ local function define_signs()
   vim.cmd([[sign define comfy-current-line text=]] .. M.config.current_line_label .. [[ texthl=CursorLineNr]])
 end
 
+local function unplace_signs()
+  vim.cmd([[sign unplace * group=comfy]])
+end
+
 local function place_signs()
   local current_file = vim.fn.expand('%')
   if current_file == nil or current_file == '' then return end
 
-  vim.cmd([[sign unplace * group=comfy]])
+  unplace_signs()
 
   local current_line = vim.fn.line('.')
   local current_id = 1
@@ -77,23 +85,69 @@ local function place_signs()
   vim.cmd([[sign place ]] .. current_id .. [[ line=]] .. current_line .. [[ name=comfy-current-line group=comfy]])
 end
 
-vim.api.nvim_create_autocmd({ 'BufEnter', 'CursorMoved', 'CursorMovedI' }, {
-  group = vim.api.nvim_create_augroup("comfy-signs", {}),
-  pattern = { '*' },
-  callback = function()
-    place_signs()
+function M.enable_line_numbers()
+  if enabled then
+    return
   end
-})
+
+  for index, label in ipairs(M.config.labels) do
+    vim.keymap.set({ 'n', 'v', 'o' }, label .. M.config.up_key, index .. 'k', { noremap = true })
+    vim.keymap.set({ 'n', 'v', 'o' }, label .. M.config.down_key, index .. 'j', { noremap = true })
+  end
+
+  vim.api.nvim_create_autocmd({ 'BufEnter', 'CursorMoved', 'CursorMovedI' }, {
+    group = vim.api.nvim_create_augroup(AUTOCMD_GROUP, {}),
+    pattern = { '*' },
+    callback = function()
+      place_signs()
+    end
+  })
+
+  place_signs()
+  enabled = true
+end
+
+function M.disable_line_numbers()
+  if not enabled then
+    return
+  end
+
+  for index, label in ipairs(M.config.labels) do
+    vim.keymap.del({ 'n', 'v', 'o' }, label .. M.config.up_key)
+    vim.keymap.del({ 'n', 'v', 'o' }, label .. M.config.down_key)
+  end
+
+  vim.api.nvim_del_augroup_by_name(AUTOCMD_GROUP)
+  unplace_signs()
+  enabled = false
+end
 
 function M.setup(config)
   M.config = vim.tbl_deep_extend("force", M.config, config or {})
 
+  vim.api.nvim_create_user_command(
+    'ComfyLineNumbers',
+    function(args)
+      if args.args == "enable" then
+        M.enable_line_numbers()
+      elseif args.args == "disable" then
+        M.disable_line_numbers()
+      elseif args.args == "toggle" then
+        if enabled then
+          M.disable_line_numbers()
+        else
+          M.enable_line_numbers()
+        end
+      else
+        print("Invalid argument.")
+      end
+    end,
+    { nargs = 1 }
+  )
+
   define_signs()
 
-  for index, label in ipairs(M.config.labels) do
-    vim.keymap.set({ 'n', 'v', 'o', 's' }, label .. M.config.up_key, index .. 'k', { noremap = true })
-    vim.keymap.set({ 'n', 'v', 'o', 's' }, label .. M.config.down_key, index .. 'j', { noremap = true })
-  end
+  M.enable_line_numbers()
 end
 
 return M
